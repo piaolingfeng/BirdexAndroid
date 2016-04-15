a60c84bb881a6e5777f9e10c9f7c6ba6815a0800
hw1989 <hw_hw1989@163.com>
2016-04-15 15:19:51 +0800

    Merge branch 'sprint3' of http://git.birdex.cn/zhuangchuming/Birdex into sprint3
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/birdex/bird/activity/InventoryActivity.java
    #	app/src/main/java/com/birdex/bird/adapter/InventoryAdapter.java
    #	app/src/main/java/com/birdex/bird/adapter/InventoryWillInAdapter.java
    #	app/src/main/java/com/birdex/bird/api/BirdApi.java
    #	app/src/main/java/com/birdex/bird/widget/TitleView.java
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/dimens.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    BMOBILE-38  使用自定义消息，将消息解析，存入本地数据库

--- app/src/main/java/com/birdex/bird/MyApplication.java
+++ app/src/main/java/com/birdex/bird/MyApplication.java
@@ -15,6 +15,7 @@ import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.birdex.bird.entity.User;
+import com.birdex.bird.service.NotificationService;
 import com.birdex.bird.util.Constant;
 import com.birdex.bird.util.CrashHandler;
 import com.loopj.android.http.AsyncHttpClient;
@@ -119,63 +120,64 @@ public class MyApplication extends Application {
         device_info = getDevice_info();
 
         mPushAgent = PushAgent.getInstance(this);
+        mPushAgent.setPushIntentServiceClass(NotificationService.class);
         mPushAgent.setDebugMode(true);
 
-        UmengMessageHandler messageHandler = new UmengMessageHandler(){
-            /**
-             * 参考集成文档的1.6.3
-             * http://dev.umeng.com/push/android/integration#1_6_3
-             * */
-            @Override
-            public void dealWithCustomMessage(final Context context, final UMessage msg) {
-                new Handler().post(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        // TODO Auto-generated method stub
-                        // 对自定义消息的处理方式，点击或者忽略
-                        boolean isClickOrDismissed = true;
-                        if(isClickOrDismissed) {
-                            //自定义消息的点击统计
-                            UTrack.getInstance(getApplicationContext()).trackMsgClick(msg);
-                        } else {
-                            //自定义消息的忽略统计
-                            UTrack.getInstance(getApplicationContext()).trackMsgDismissed(msg);
-                        }
-                        Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
-
-            /**
-             * 参考集成文档的1.6.4
-             * http://dev.umeng.com/push/android/integration#1_6_4
-             * */
-            @Override
-            public Notification getNotification(Context context,
-                                                UMessage msg) {
-                switch (msg.builder_id) {
-                    case 1:
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(), R.layout.notification_view);
-                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
-                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
-                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
-                        myNotificationView.setImageViewResource(R.id.notification_small_icon, getSmallIconId(context, msg));
-                        builder.setContent(myNotificationView)
-                                .setSmallIcon(getSmallIconId(context, msg))
-                                .setTicker(msg.ticker)
-                                .setAutoCancel(true);
-
-                        return builder.build();
-
-                    default:
-                        //默认为0，若填写的builder_id并不存在，也使用默认。
-                        return super.getNotification(context, msg);
-                }
-            }
-        };
-        mPushAgent.setMessageHandler(messageHandler);
+//        UmengMessageHandler messageHandler = new UmengMessageHandler(){
+//            /**
+//             * 参考集成文档的1.6.3
+//             * http://dev.umeng.com/push/android/integration#1_6_3
+//             * */
+//            @Override
+//            public void dealWithCustomMessage(final Context context, final UMessage msg) {
+//                new Handler().post(new Runnable() {
+//
+//                    @Override
+//                    public void run() {
+//                        // TODO Auto-generated method stub
+//                        // 对自定义消息的处理方式，点击或者忽略
+//                        boolean isClickOrDismissed = true;
+//                        if(isClickOrDismissed) {
+//                            //自定义消息的点击统计
+//                            UTrack.getInstance(getApplicationContext()).trackMsgClick(msg);
+//                        } else {
+//                            //自定义消息的忽略统计
+//                            UTrack.getInstance(getApplicationContext()).trackMsgDismissed(msg);
+//                        }
+//                        Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//            }
+//
+//            /**
+//             * 参考集成文档的1.6.4
+//             * http://dev.umeng.com/push/android/integration#1_6_4
+//             * */
+//            @Override
+//            public Notification getNotification(Context context,
+//                                                UMessage msg) {
+//                switch (msg.builder_id) {
+//                    case 1:
+//                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+//                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(), R.layout.notification_view);
+//                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+//                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+//                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
+//                        myNotificationView.setImageViewResource(R.id.notification_small_icon, getSmallIconId(context, msg));
+//                        builder.setContent(myNotificationView)
+//                                .setSmallIcon(getSmallIconId(context, msg))
+//                                .setTicker(msg.ticker)
+//                                .setAutoCancel(true);
+//
+//                        return builder.build();
+//
+//                    default:
+//                        //默认为0，若填写的builder_id并不存在，也使用默认。
+//                        return super.getNotification(context, msg);
+//                }
+//            }
+//        };
+//        mPushAgent.setMessageHandler(messageHandler);
 
         /**
          * 该Handler是在BroadcastReceiver中被调用，故
@@ -183,16 +185,16 @@ public class MyApplication extends Application {
          * 参考集成文档的1.6.2
          * http://dev.umeng.com/push/android/integration#1_6_2
          * */
-        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler(){
-            @Override
-            public void dealWithCustomAction(Context context, UMessage msg) {
-                Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
-            }
-        };
-        //使用自定义的NotificationHandler，来结合友盟统计处理消息通知
-        //参考http://bbs.umeng.com/thread-11112-1-1.html
-        //CustomNotificationHandler notificationClickHandler = new CustomNotificationHandler();
-        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+//        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler(){
+//            @Override
+//            public void dealWithCustomAction(Context context, UMessage msg) {
+//                Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+//            }
+//        };
+//        //使用自定义的NotificationHandler，来结合友盟统计处理消息通知
+//        //参考http://bbs.umeng.com/thread-11112-1-1.html
+//        //CustomNotificationHandler notificationClickHandler = new CustomNotificationHandler();
+//        mPushAgent.setNotificationClickHandler(notificationClickHandler);
         mPushAgent.onAppStart();
         //开启推送并设置注册的回调处理
         mPushAgent.enable(new IUmengRegisterCallback() {
@@ -209,8 +211,8 @@ public class MyApplication extends Application {
             }
         });
         device_token = UmengRegistrar.getRegistrationId(this);
-        String pkgName = getApplicationContext().getPackageName();
-        Log.e("pkgName", pkgName);
+//        String pkgName = getApplicationContext().getPackageName();
+//        Log.e("pkgName", pkgName);
         Log.e("device_token", device_token);
 //		if (MiPushRegistar.checkDevice(this)) {
 //            MiPushRegistar.register(this, "2882303761517400865", "5501740053865");
